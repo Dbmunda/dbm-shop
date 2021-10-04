dd3464eb34ebe1e759b2637044de6455e9dec2dc
DBm <50243408+Dbmunda@users.noreply.github.com>
2021-10-04 13:51:01 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = ({totalItems}) => {
                 <Toolbar>
                     <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
                         <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
-                         DBM SHOP
+                         My Shop
                     </Typography>
                     <div className={classes.grow} />
                     {location.pathname === '/'&& (
